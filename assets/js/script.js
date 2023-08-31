68bec15dac8775ea31eb5446a86880a9ce89b070
sudhamsh99 <92301797+sudhamsh99@users.noreply.github.com>
2023-08-31 16:32:15 +0530

    s

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var swiper1 = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+});
+
+
+
